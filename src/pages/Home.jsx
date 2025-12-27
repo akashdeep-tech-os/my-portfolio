33a2605ff0e48f88bba4f986649697beb805da19
Akash Deep <akashdeeptech@gmail.com>
2025-12-27 19:32:04 +0530

    Final working portfolio setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,21 @@
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Skills from "../components/Skills";
-import Project from "../components/Project";
-import Work from "../components/Work";
-import Contact from "../components/Contact";
-
+import React from 'react'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Project from '../components/Project'
+import Work from '../components/Work'
+import Contact from '../components/Contact'
 const Home = () => {
   return (
-    <>
-      <section id="home">
-        <Hero />
-      </section>
+    <div>
+      <Hero />
+      <About />
+      <Skills />
+      <Project />
+      <Work />
+      <Contact />
+    </div>
+  )
+}
 
-      <section id="about">
-        <About />
-      </section>
-
-      <section id="skills">
-        <Skills />
-      </section>
-
-      <section id="project">
-        <Project />
-      </section>
-
-      <section id="experience">
-        <Work />
-      </section>
-
-      <section id="contact">
-        <Contact />
-      </section>
-    </>
-  );
-};
-
-export default Home;
+export default Home
